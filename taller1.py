05abc47f2d9e736e0d10523b9f38df181c173ee8
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-09-03 23:18:27 -0500

    Falta relaciones coeficientes

--- taller1.py
+++ taller1.py
@@ -2,79 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 G = 6.674*10**(-11)
-dsuelo = 50.0
+dsuelo = 2700.0
 
 #Caso Esfera
 def anomEsfera(x,z, desf):
 	r= 10.0
 	return (4/3)*np.pi*G*(r**3)*(desf-dsuelo)*z/(((x)**2+(z)**2)**(3/2))
 
-x = np.linspace(-80,80,100)
+x = np.linspace(-120,120,1000)
 
 z1 = 20.0
 z2 = 40.0
 z3 = 60.0
-esf1 = anomEsfera(x,z1,100.0)
-esf2 = anomEsfera(x,z2,100.0)
-esf3 = anomEsfera(x,z3,100.0)
-Diapiro = anomEsfera(x,z2,25.0)
+esf1 = anomEsfera(x,z1,3500.0)
+esf2 = anomEsfera(x,z2,3500.0)
+esf3 = anomEsfera(x,z3,3500.0)
+Diapiro = anomEsfera(x,z2,2000.0)
 
-ze1 = esf1/max(esf1)
-ze2 = esf2/max(esf2)
-ze3 = esf3/max(esf3)
-zDiapiro = Diapiro/abs(min(Diapiro))
 
-plt.plot(x,ze1, label = "z = 20")
-plt.plot(x,ze2, label = "z = 40")
-plt.plot(x,ze3, label = "z = 60")
-plt.plot(x,zDiapiro, label = "z = 20, Diapiro(densidad menor)")
+plt.plot(x,esf1, label = "z = 20")
+plt.plot(x,esf2, label = "z = 40")
+plt.plot(x,esf3, label = "z = 60")
+plt.plot(x,Diapiro, label = "z = 40, Diapiro(densidad menor)")
 plt.legend(loc=0)
+plt.xlabel("x(m)")
+plt.ylabel("g(mGal)")
+plt.title("Efecto de gravedad de una esfera")
+plt.tight_layout()
 plt.savefig("AEsfera.pdf")
 plt.close()
 
 
-#relacion xhalf y g
-ge1= max(esf1)/2
-ge2= max(esf2)/2
-ge3= max(esf3)/2
-geDip= max(Diapiro)/2
+#relacion xhalf y z
+xhalf1 = 2*(2*np.log(2))**0.5*np.std(esf1)
+xhalf2 = 2*(2*np.log(2))**0.5*np.std(esf2)
+xhalf3 = 2*(2*np.log(2))**0.5*np.std(esf3)
+xhalfDip = 2*(2*np.log(2))**0.5*np.std(Diapiro)
 
-v = 0
-val1 = 0
-val2 = 0
-val3 = 0
-valDip = 0
-while v<len(x):
-	if x[v]==ge1:
-		val1 = v
-	v+=1
-
-while v<len(x):
-	if x[v]==ge2:
-		val2 = v
-	v+=1
-
-while v<len(x):
-	if x[v]==ge3:
-		val3 = v
-	v+=1
-
-while v<len(x):
-	if x[v]==geDip:
-		valDip = v
-	v+=1
-
-x1med = 2*val1
-coesf1 = x1med/z1
-
-x2med = 2*val2
-coesf2 = x2med/z2
-
-x3med = 2*val3
-coesf3 = x3med/z3
-
-xDipmed = 2*valDip
-coesfDip = xDipmed/z2
+#print xhalf1, xhalf2, xhalf3, xhalfDip
+#NOSALEEE
 
 
 #Caso Cilindro
@@ -82,39 +48,95 @@ def anomCil(x,z, dcil):
 	r= 10.0
 	return 2*np.pi*G*(r**2)*(dcil-dsuelo)*z/((x)**2+(z)**2)
 
-cil1 = anomCil(x,20.0, 100.0)
-cil2 = anomCil(x,40.0, 100.0)
-cil3 = anomCil(x,60.0, 100.0)
+cil1 = anomCil(x,z1, 10000.0)
+cil2 = anomCil(x,z2, 10000.0)
+cil3 = anomCil(x,z3, 10000.0)
 
-zcil1 = cil1/max(cil1)
-zcil2 = cil2/max(cil2)
-zcil3 = cil3/max(cil3) 
-
-plt.plot(x,zcil1, label = "P. Somera")
-plt.plot(x,zcil2, label = "P. Media")
-plt.plot(x,zcil3, label = "P. Profunda")
+plt.plot(x,cil1, label = "P. Somera")
+plt.plot(x,cil2, label = "P. Media")
+plt.plot(x,cil3, label = "P. Profunda")
 plt.legend(loc=0)
+plt.xlabel("x(m)")
+plt.ylabel("g(mGal)")
+plt.title("Efecto de gravedad de un cilindro horizontal")
+plt.tight_layout()
 plt.savefig("ACilindro.pdf")
 plt.close()
 
+#relacion xhalf y z cilindro
+xm1 = 2*(2*np.log(2))**0.5*np.std(cil1)
+xm2 = 2*(2*np.log(2))**0.5*np.std(cil2)
+xm3 = 2*(2*np.log(2))**0.5*np.std(cil3)
+
+print z1/xm1, z2/xm2, z3/xm3
+
 #Caso Losa
 def anomLosa(x, h, z, dLosa):
-	r = 10.0
 	return 2*G*(dLosa-dsuelo)*h*(np.pi/2 + np.arctan(x/z))
 
-L1 = anomLosa(x, 20.0, 10.0, 100.0)
-L2 = anomLosa(x, 30.0, 20.0, 20.0)
-L3 = anomLosa(x, 40.0, 50.0, 150.0)
+L1 = anomLosa(x, 20.0, 10.0, 10000.0)
+L2 = anomLosa(x, 30.0, 20.0, 5000.0)
+L3 = anomLosa(x, 40.0, 50.0, 7000.0)
 
-zL1 = L1/max(L1)
-zL2 = L2/max(L2)
-zL3 = L3/max(L3)
-
-plt.plot(x,zL1, label = "h = 20, z = 10, densidad = 100 ")
-plt.plot(x,zL2, label = "h = 30, z = 20, densidad = 20")
-plt.plot(x,zL3, label = "h = 40, z = 50, densidad = 150")
+#Grafica efecto de gravedad de una losa
+plt.plot(x,L1, label = "h = 20, p. somera, densidad = 10000 ")
+plt.plot(x,L2, label = "h = 30, p. media, densidad = 5000")
+plt.plot(x,L3, label = "h = 40, p. profunda, densidad = 7000")
 plt.legend(loc=0)
+plt.xlabel("x(m)")
+plt.ylabel("g(mGal)")
+plt.title("Efecto de gravedad de una losa")
+plt.tight_layout()
 plt.savefig("ALosa.pdf")
+plt.close()
+
+#Primera derivada
+def Dx(x, h, z, dLosa):
+	return 2*G*(dLosa-dsuelo)*h*(z/(x*x+z*z))
+
+dxL1 = Dx(x, 20.0, 10.0, 10000.0)
+dxL2 = Dx(x, 30.0, 20.0, 5000.0)
+dxL3 = Dx(x, 40.0, 50.0, 7000.0)
+
+#Segundas derivadas
+def D2x(x, h, z, dLosa):
+	return 2*G*(dLosa-dsuelo)*h*(-2*x*z/(x*x+z*z)**2)
+
+d2xL1 = D2x(x, 20.0, 10.0, 10000.0)
+d2xL2 = D2x(x, 30.0, 20.0, 5000.0)
+d2xL3 = D2x(x, 40.0, 50.0, 7000.0)
+
+#Grafica derivadas
+plt.subplot(3,1,1)
+plt.plot(x,dxL1, label = "primera derivada ")
+plt.plot(x,d2xL1, label = "segunda derivada ")
+plt.title("Anomalia 1 (h = 20, p. somera, densidad = 10000)")
+plt.legend(loc=0)
+plt.xlabel("x(m)")
+plt.ylabel("g(mGal)")
+
+plt.subplot(3,1,2)
+plt.plot(x,dxL2, label = "primera derivada ")
+plt.plot(x,d2xL2, label = "segunda derivada ")
+plt.title("Anomalia 2 (h = 30, p. media, densidad = 5000)")
+plt.legend(loc=0)
+plt.xlabel("x(m)")
+plt.ylabel("g(mGal)")
+
+plt.subplot(3,1,3)
+plt.plot(x,dxL3, label = "primera derivada ")
+plt.plot(x,d2xL3, label = "segunda derivada ")
+plt.title("Anomalia 3 (h = 40, p. profunda, densidad = 7000)")
+plt.legend(loc=0)
+plt.xlabel("x(m)")
+plt.ylabel("g(mGal)")
+
+plt.tight_layout()
+plt.savefig("derivadas.pdf")
+plt.close()
+
+
+
 
 
 
